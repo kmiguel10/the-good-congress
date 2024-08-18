7591d5701003064fe14edb5ba7e8cf14550d19f1
Kent Miguel <kentmiguel10@gmail.com>
2024-08-18 13:27:46 -0400

    Update opengovService.ts

--- app/services/opengovService.ts
+++ app/services/opengovService.ts
@@ -1,21 +1,22 @@
 const BASE_URL = "https://api.congress.gov/v3";
 
+// Ensure the function returns the result of fetchData
 export const getCurrentCongress = async () => {
-  fetchData("/congress/current");
+  return await fetchData("/congress/current");
 };
 
 const fetchData = async (endpoint: string) => {
-  console.log("API: ", process.env.NEXT_PUBLIC_OPEN_CONGRESS_GOV_API_KEY);
+  console.log("API Key: ", process.env.NEXT_PUBLIC_OPEN_CONGRESS_GOV_API_KEY);
   try {
     const response = await fetch(
       `${BASE_URL}${endpoint}?api_key=${process.env.NEXT_PUBLIC_OPEN_CONGRESS_GOV_API_KEY}`
     );
     if (!response.ok) {
-      throw new Error("Failed to fetch data");
+      throw new Error(`Failed to fetch data: ${response.statusText}`);
     }
-
     return await response.json();
   } catch (error) {
     console.log("Error fetching data in service layer: ", error);
+    throw error; // Ensure the error is re-thrown so it can be caught in the GET function
   }
 };

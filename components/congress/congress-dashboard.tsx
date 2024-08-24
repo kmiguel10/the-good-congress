51f4354bbfdd8a54deafd3c3498e136cdaa7ff92
Kent Miguel <kentmiguel10@gmail.com>
2024-08-24 12:20:37 -0400

    refactored to add useCongress hook and added skeleton

--- components/congress/congress-dashboard.tsx
+++ components/congress/congress-dashboard.tsx
@@ -3,59 +3,29 @@ import React, { useEffect, useState } from "react";
 import { TableSkeleton } from "../global/table-skeleton";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import CongressTable from "./members-table/page";
+import { useCongressMembers } from "@/app/hooks/useCongressMembers";
 
 interface Props {
   currentCongress: string;
 }
 
 export const CongressDashboard: React.FC<Props> = ({ currentCongress }) => {
-  const [congressTableData, setCongressTableData] = useState<
-    CongressMemberTable[]
-  >([]);
-  const [isCongressTableLoading, setIsCongressTableLoading] = useState(false);
-
-  //filter members by putting it in a new shape
-
-  //Fetch the current members of this congress
-  useEffect(() => {
-    const fetchCurrentMembers = async () => {
-      try {
-        setIsCongressTableLoading(true);
-        const response = await fetch(
-          `/api/opengov/member/congress/${currentCongress}`
-        );
-        const data: Member[] = await response.json();
-
-        const filteredMembers = getCongressTableMembers(data);
-        console.log("Filtered members: ", filteredMembers);
-        setCongressTableData(filteredMembers);
-        setIsCongressTableLoading(false);
-      } catch (error) {
-        console.error("Error fetching current members: ", error);
-        setIsCongressTableLoading(false);
-      }
-    };
-
-    if (currentCongress) {
-      fetchCurrentMembers();
-    }
-  }, [currentCongress]);
+  const { congressTableData, isLoading, isDataReady } =
+    useCongressMembers(currentCongress);
 
   return (
     <div className="flex-1 space-y-4 p-8 pt-6">
       <div className="flex items-center justify-between space-y-2">
         <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-        <div className="flex items-center space-x-2"></div>
       </div>
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 "></div>
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-8">
         <Card className="col-span-4">
           <CardHeader>
             <CardTitle>Members</CardTitle>
           </CardHeader>
           <CardContent className="pl-2">
-            {isCongressTableLoading && <TableSkeleton />}
-            {!isCongressTableLoading && (
+            {isLoading && <TableSkeleton />}
+            {!isLoading && isDataReady && congressTableData && (
               <CongressTable members={congressTableData} />
             )}
           </CardContent>
--- components/global/table-skeleton.tsx
+++ components/global/table-skeleton.tsx
@@ -2,11 +2,31 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export function TableSkeleton() {
   return (
-    <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+    <div className="space-y-4">
+      <div className="flex items-center py-4">
+        <Skeleton className="h-10 w-[300px]" />
+      </div>
+
+      <div className="rounded-md border">
+        <div className="p-4">
+          <div className="grid grid-cols-5 gap-4">
+            {[...Array(5)].map((_, index) => (
+              <Skeleton key={index} className="h-6" />
+            ))}
+          </div>
+          {[...Array(5)].map((_, rowIndex) => (
+            <div key={rowIndex} className="mt-4 grid grid-cols-5 gap-4">
+              {[...Array(10)].map((_, cellIndex) => (
+                <Skeleton key={cellIndex} className="h-6" />
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Skeleton className="h-9 w-[80px]" />
+        <Skeleton className="h-9 w-[80px]" />
       </div>
     </div>
   );
